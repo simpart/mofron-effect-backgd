1f68d50bce4eca31f9fe8a625a8f1740b294aa8b
simpart <simpart>
2019-01-20 13:29:08 +0000

    add offset

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ mofron.effect.Backgd = class extends mofron.Effect {
         try {
             super();
             this.name('Backgd');
+            this.prmMap('offset');
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -31,7 +32,12 @@ mofron.effect.Backgd = class extends mofron.Effect {
      */
     enable (tgt) {
         try {
-            tgt.size('100%', '100%');
+            let off = this.offset();
+            let val = [
+                (100 + off[0]) + '%',
+                (100 + off[1]) + '%'
+            ];
+            tgt.size(val[0], val[1]);
             tgt.style({ 'position' : 'fixed' });
         } catch (e) {
             console.error(e.stack);
@@ -49,6 +55,35 @@ mofron.effect.Backgd = class extends mofron.Effect {
             tgt.size('0%', '0%');
             tgt.style({ 'position' : null });
         } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    offset (x, y) {
+        try {
+            if (undefined === x) {
+                /* getter */
+                return [ this.offset_x(), this.offset_y() ];
+            }
+            /* setter */
+            this.offset_x(x);
+            this.offset_y(y);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    offset_x (prm) {
+        try { return this.member('offset_x', 'number', prm, 0); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    offset_y (prm) {
+        try { return this.member('offset_y', 'number', prm, 0); } catch (e) {
             console.error(e.stack);
             throw e;
         }
